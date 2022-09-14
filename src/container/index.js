87d1b2df98fbaab6d68142fb2b6672182b33678d
danielcrespo <necromancer.gd@gmail.com>
2022-09-13 19:56:39 -0500

    handle Delete components fixed

--- src/container/index.js
+++ src/container/index.js
@@ -7,8 +7,10 @@ export class ContainerForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.inputSelected = '';
-        this.extraComponents = [];
+        this.components = new Map();
+        this.componentsLength = 0;
+        this.componentIndex = 0
+        this.prevIndex = ''
 
         this.state = {
             value: {
@@ -16,7 +18,7 @@ export class ContainerForm extends React.Component {
                 select: '',
                 radio: ''
             },
-            components: ComponentsJson.components
+            components: []//ComponentsJson.components
         }
 
         this.handleChangeText = this.handleChangeText.bind(this);
@@ -24,17 +26,47 @@ export class ContainerForm extends React.Component {
         this.handleChangeOptions = this.handleChangeOptions.bind(this);
     }
 
-    // componentDidUpdate(prevProps,prevState) {
-    //     if (this.inputSelected = this.extraComponents[0].type) {
-    //         console.log('form didUpdate',prevProps, prevState, this.extraComponents);
-    //         this.setState({
-    //             components: [
-    //                 ...(prevState.components),
-    //                 this.extraComponents
-    //             ]
-    //         })
-    //     }
-    // }
+    componentDidMount() {
+        let result = [];
+        ComponentsJson.components.forEach((component) => {
+            this.components.set(component.type, component);
+        });
+
+        if (this.components.size > 0) {
+            this.components.forEach((value, key) => {
+                result.push(value);
+            })
+        }
+
+        if (result) {
+            console.log('componentDidMount', result, this.components);
+            this.setState({
+                components: result,
+            })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.components.length < this.componentsLength) {
+            console.log('componentDidUpdate', this.componentsLength, prevState.components, this.components);
+            let result = [];
+            this.components.forEach((component) => {
+                result.push(component);
+            });
+
+            if (result) {
+                this.setState({
+                    components: result,
+                })
+            }
+
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if (this.handleAddComponents)
+            return true
+    }
 
     handleChangeText(ev) {
         ev.preventDefault();
@@ -78,37 +110,75 @@ export class ContainerForm extends React.Component {
         })
     }
 
-    handleAddComponents = (ev, component) => {
+    handleAddComponents = (ev, component, index) => {
+        let count = 0;
         const {components} = ComponentsJson;
-        const newComponent = components.find((item) => item.type === component);
 
-        if (ev.target.name === component)
-            this.setState((prevState) => ({
-                components: [
-                    ...prevState.components,
-                    newComponent
-                ]
-            }));
-        this.inputSelected = component;
+        const {name} = ev.target;
+
+        const newComponent = components.find((item) => item.type === component)
+        const componentCopy = Object.assign({}, newComponent);
+        const componentExistent = this.components.get(`${component}_${name}`);
+
+        if (componentExistent){
+            count++;
+        this.componentIndex = count;
+            this.components.set(`${component}_${name}_${count}`, componentCopy);
+        }
+        else
+            this.components.set(`${component}_${name}`, componentCopy);
+
+        this.componentsLength = this.components.size;
+        console.log('handleAddComponent', this.components, name, count);
+
+        this.setState((prevState) => {
+            let result = [];
+            const {components} = prevState;
+
+            if (components.length < this.components.size) {
+                this.components.forEach((value, key) => {
+                    result.push(value);
+                });
+                if (result) {
+                    return {
+                        components: result,
+                    }
+                }
+            }
+        })
     }
 
-    handleRemoveComponents = (ev, component) => {
+    handleRemoveComponents = (ev, component, index) => {
+        let count = 0;
+
         ev.preventDefault()
-        const {components} = ComponentsJson;
-        const removeComponent = components.find((item) => item.type === component);
 
-        if (ev.target.name === component)
-            this.setState((prevState) => {
-                const newState = prevState.components.filter((item) => item.type !== component );
-                console.log('indexComponent', newState)
-                return {
-                    components:[
-                        newState[0]
-                    ]
+        const {name} = ev.target;
+        const componentFound = this.components.get(`${component}_${name}`);
 
+        if (componentFound) {
+            this.components.delete(`${component}_${name}`);
+        }
+        else
+            this.components.delete(`${component}_${name}_${index}`);
+
+        console.log('handleRemoveComponents', this.components, component, index);
+        this.componentsLength = this.components.size;
+        this.setState((prevState) => {
+            let result = [];
+            const {components} = prevState;
+
+            if (components.length > this.components.size) {
+                this.components.forEach((value, key) => {
+                    result.push(value);
+                });
+                if (result) {
+                    return {
+                        components: result,
+                    }
                 }
-            });
-        this.inputSelected = component;
+            }
+        });
     }
 
     render() {
@@ -118,38 +188,47 @@ export class ContainerForm extends React.Component {
                 <main>
                     <form>
                         {
-                            this.state.components.map((component, i) => {
-                                const {attributes} = component;
-                                const Component = KeyComponents[component.type];
+                            this.state.components ?
+                                this.state.components.map((component, i) => {
+                                    const {attributes} = component;
+                                    const Component = KeyComponents[component.type];
+                                    const index = this.componentIndex;
 
-                                return (
-                                    <>
-                                        <fieldset key={`${attributes?.keycomponent}_fieldset_${i}`} style={{border: 0}}>
-                                            <button key={`${attributes?.keycomponent}_button_remove_${i}`}
-                                                    name={component.type}
-                                                    className='removeComponentsBtn'
-                                                    onClick={(e) => this.handleRemoveComponents(e, component.type)}
-                                            >
-                                                x
-                                            </button>
-                                            <Component
-                                                key={`${attributes?.keycomponent}_${i}`}
-                                                {...{
-                                                    ...component,
-                                                    attributes: {
-                                                        ...attributes
-                                                    },
-                                                    handlers: {
-                                                        handleChangeText: this.handleChangeText,
-                                                        handleChangeSelect: this.handleChangeSelect,
-                                                        handleChangeOption: this.handleChangeOptions
-                                                    }
-                                                }}
-                                            />
-                                        </fieldset>
-                                    </>
-                                )
-                            })
+                                    return (
+                                        <>
+                                            <fieldset key={`${attributes?.keycomponent}_fieldset_${i}`}
+                                                      style={{border: 0}}>
+
+                                                {
+                                                    i > 2 ? (
+                                                        <button key={`${attributes?.keycomponent}_button_remove_${i}`}
+                                                                name={component.type}
+                                                                className='removeComponentsBtn'
+                                                                onClick={(e) => this.handleRemoveComponents(e, component.type, index)}
+                                                        >
+                                                            x
+                                                        </button>
+                                                    ) : null
+                                                }
+                                                <Component
+                                                    key={`${attributes?.keycomponent}_${i}`}
+                                                    {...{
+                                                        ...component,
+                                                        attributes: {
+                                                            ...attributes
+                                                        },
+                                                        handlers: {
+                                                            handleChangeText: this.handleChangeText,
+                                                            handleChangeSelect: this.handleChangeSelect,
+                                                            handleChangeOption: this.handleChangeOptions
+                                                        }
+                                                    }}
+                                                />
+                                            </fieldset>
+                                        </>
+                                    )
+                                })
+                                : null
                         }
                     </form>
 
@@ -166,7 +245,7 @@ export class ContainerForm extends React.Component {
                                                 key={`button_list_components_${i}`}
                                                 name={component.type}
                                                 className='addComponentsBtn'
-                                                onClick={(e) => this.handleAddComponents(e, component.type)}
+                                                onClick={(e) => this.handleAddComponents(e, component.type, i)}
                                                 type='button'
                                             >
                                                 {component.type}
